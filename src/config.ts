8fcbb2dc618b25f490f9f8e05ae301ed096dbf08
deokgoo <kkddgg1001@gmail.com>
2021-05-08 19:29:42 +0900

    temaplte

--- src/config.ts
+++ src/config.ts
@@ -15,13 +15,13 @@ type Config = {
 }
 
 const config: Config = {
-  avatarImg: 'https://avatars.githubusercontent.com/u/20182365?v=4',
-  author: 'deokgoo',
-  description: 'tech blog all web knowledge',
+  avatarImg: '',
+  author: '',
+  description: '',
   link: {
-    mail: 'kkddgg1001@gmail.com',
-    github: 'https://github.com/deokgoo',
-    linkedin: 'https://www.linkedin.com/in/kim-deokgoo-51b583155/',
+    mail: '',
+    github: '',
+    linkedin: '',
     facebook: '',
     instagram: '',
     twitter: '',
